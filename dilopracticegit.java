ee5c70edcfc264fc7fc96ce238031924f5d75ef4
Dil-Awar Harun El R. <dilawarharun27@gmail.com>
2024-11-18 11:20:39 +0700

    message

---
+++ dilopracticegit.java
@@ -0,0 +1,6 @@
+public class dilopracticegit{
+    public static void main(String[] args) {
+        System.out.println("dilo kamu lagi latihan ya ");
+        System.out.println("iya lagi mencoba memahami ");
+    }
+}
